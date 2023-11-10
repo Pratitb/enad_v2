f2a8fdb33c3e8a43d862e2917bf8a12f4c236f30
pratitb <pratit.bangdiwala7@gmail.com>
2023-11-11 01:07:45 +0530

    links message

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 AOS.init({
-    duration: 2000,
+    duration: 1500,
 });
 
 const heroCta = document.querySelector('.hero_cta')
 const industriesSec = document.querySelector('.industries')
+const callCta = document.querySelector('.call_cta')
+const callCtaPopup = document.querySelector('.call_cta_popup')
+const navLinks = document.querySelectorAll('.nav_links')
 
 heroCta.addEventListener('click', scrollToIndustries)
+callCta.addEventListener('click', showWaitMessage)
+callCtaPopup.addEventListener('click', closeCallCtaPopup)
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', showWaitMessage)
+});
 
 function scrollToIndustries(){
     industriesSec.scrollIntoView()
+}
+function showWaitMessage(){
+    callCtaPopup.classList.add('active_flex')
+}
+function closeCallCtaPopup(callClick){
+    if(!(callClick.target.classList.contains('call_cta_container'))){
+        callCtaPopup.classList.remove('active_flex') 
+    }
 }
