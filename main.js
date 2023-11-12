215fb2d8e25e48e6a912bb5134b2af32a93c0f63
pratitb <pratit.bangdiwala7@gmail.com>
2023-11-12 16:15:44 +0530

    animations, footer

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-AOS.init({
-    duration: 1200,
-});
+// AOS.init({
+//     duration: 1200,
+// });
 
+const heroSection = document.querySelector('.hero')
+const heroCont = document.querySelector('.hero_container')
 const heroCta = document.querySelector('.hero_cta')
 const industriesSec = document.querySelector('.industries')
 const callCta = document.querySelector('.call_cta')
 const callCtaPopup = document.querySelector('.call_cta_popup')
 const navLinks = document.querySelectorAll('.nav_links')
 
+document.addEventListener('DOMContentLoaded', animateHero)
 heroCta.addEventListener('click', scrollToIndustries)
 callCta.addEventListener('click', showWaitMessage)
 callCtaPopup.addEventListener('click', closeCallCtaPopup)
@@ -15,6 +18,12 @@ navLinks.forEach(navLink => {
     navLink.addEventListener('click', showWaitMessage)
 });
 
+function animateHero(){
+    heroSection.classList.add('active_hero')
+    setTimeout(() => {
+        heroCont.classList.add('hero_container_active')
+    }, 1800);
+}
 function scrollToIndustries(){
     industriesSec.scrollIntoView()
 }
